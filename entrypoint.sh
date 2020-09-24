f78d2e9bd8b144b43af1cc2a598e919b23e4a028
Nathan J. Mehl <n@oden.io>
2020-09-24 11:19:34 -0400

    skip fetch

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,12 +22,13 @@ CLIENT_EMAIL="$(jq -r .client_email <<< "${GOOGLE_CREDENTIALS}")"
 
 git_setup
 git remote -v
-git remote update
-git fetch --all
+# this should have been done by the checkout step
+#git remote update
+#git fetch --all
 # check out the source branch at exactly the ref where we were triggered
 SOURCE_BRANCH="tmp-$(basename "${GITHUB_REF}")"
 DESTINATION_BRANCH="$(basename "${GITHUB_REF}")"
-git checkout -b "${SOURCE_BRANCH}" "${PUSH_SHA}"
+git checkout -b "${SOURCE_BRANCH}" "${GITHUB_SHA}"
 
 git remote add destination "${DESTINATION_REPO}"
 git config --replace-all credential.helper '!gcloud auth git-helper --account='"${CLIENT_EMAIL}"' --ignore-unknown $@'
