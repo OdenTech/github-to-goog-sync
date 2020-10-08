5cd47123106dbb137e4ce853ae60bafd4654e92b
Nathan J. Mehl <n@oden.io>
2020-10-08 16:12:37 -0400

    pick creds based on branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,17 +17,28 @@ EOF
   git config --global user.name "$GITHUB_ACTOR"
 }
 
+SOURCE_BRANCH="tmp-$(basename "${GITHUB_REF}")"
+DESTINATION_BRANCH="$(basename "${GITHUB_REF}")"
+
+# we really need some less fragile way to establish this mapping
+case DESTINATION_BRANCH in
+  master)
+    GOOGLE_CREDENTIALS="${GOOGLE_CREDENTIALS_ODEN_PRODUCTION}"
+    GOOGLE_SOURCE_REPO_URL="${GOOGLE_SOURCE_REPO_URL_ODEN_PRODUCTION}"
+    ;;
+  *)
+    GOOGLE_CREDENTIALS="${GOOGLE_CREDENTIALS_ODEN_QA}"
+    GOOGLE_SOURCE_REPO_URL="${GOOGLE_SOURCE_REPO_URL_ODEN_QA}"
+    ;;
+esac
 
 CLIENT_EMAIL="$(jq -r .client_email <<< "${GOOGLE_CREDENTIALS}")"
 echo "${GOOGLE_CREDENTIALS}" > /tmp/creds.json
 /google-cloud-sdk/bin/gcloud auth activate-service-account "${CLIENT_EMAIL}" --key-file=/tmp/creds.json
 
 git_setup
-git remote -v
 
 # check out the source branch at exactly the ref where we were triggered
-SOURCE_BRANCH="tmp-$(basename "${GITHUB_REF}")"
-DESTINATION_BRANCH="$(basename "${GITHUB_REF}")"
 git checkout -b "${SOURCE_BRANCH}" "${GITHUB_SHA}"
 
 git remote add destination "${GOOGLE_SOURCE_REPO_URL}"
