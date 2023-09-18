eae7cee92790b05c525fe3097788f76c76f2ece0
Nathan J. Mehl <n@oden.io>
2023-09-18 10:14:04 -0400

    clean up git auth, add ghcr deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,14 @@
 
 set -ex
 
+google_auth_setup() {
+  CLIENT_EMAIL="$(jq -r .client_email <<<"${GOOGLE_CREDENTIALS}")"
+  echo "${GOOGLE_CREDENTIALS}" >/tmp/creds.json
+  /google-cloud-sdk/bin/gcloud auth activate-service-account "${CLIENT_EMAIL}" --key-file=/tmp/creds.json
+}
+
 git_setup() {
-  cat <<- EOF > "$HOME/.netrc"
+  cat <<-EOF >"$HOME/.netrc"
 		machine github.com
 		login "$GITHUB_ACTOR"
 		password "$GITHUB_TOKEN"
@@ -15,9 +21,10 @@ EOF
 
   git config --global user.email "$GITBOT_EMAIL"
   git config --global user.name "$GITHUB_ACTOR"
-  # rf: https://github.com/actions/checkout/issues/760 this works around a new perms 
+  # rf: https://github.com/actions/checkout/issues/760 this works around a new perms
   # check added for CVE-2022-24765
   git config --global --add safe.directory /github/workspace
+  git config --global credential.'https://source.developer.google.com/'.helper '!/google-cloud-sdk/bin/gcloud auth git-helper --account='"${CLIENT_EMAIL}"' --ignore-unknown $@'
 }
 
 SOURCE_BRANCH="tmp-$(basename "${GITHUB_REF}")"
@@ -25,26 +32,22 @@ DESTINATION_BRANCH="$(basename "${GITHUB_REF}")"
 
 # we really need some less fragile way to establish this mapping
 case "${DESTINATION_BRANCH}" in
-  master)
-    GOOGLE_CREDENTIALS="${GOOGLE_CREDENTIALS_ODEN_PRODUCTION}"
-    GOOGLE_SOURCE_REPO_URL="${GOOGLE_SOURCE_REPO_URL_ODEN_PRODUCTION}"
-    ;;
-  *)
-    GOOGLE_CREDENTIALS="${GOOGLE_CREDENTIALS_ODEN_QA}"
-    GOOGLE_SOURCE_REPO_URL="${GOOGLE_SOURCE_REPO_URL_ODEN_QA}"
-    ;;
+master)
+  GOOGLE_CREDENTIALS="${GOOGLE_CREDENTIALS_ODEN_PRODUCTION}"
+  GOOGLE_SOURCE_REPO_URL="${GOOGLE_SOURCE_REPO_URL_ODEN_PRODUCTION}"
+  ;;
+*)
+  GOOGLE_CREDENTIALS="${GOOGLE_CREDENTIALS_ODEN_QA}"
+  GOOGLE_SOURCE_REPO_URL="${GOOGLE_SOURCE_REPO_URL_ODEN_QA}"
+  ;;
 esac
 
-CLIENT_EMAIL="$(jq -r .client_email <<< "${GOOGLE_CREDENTIALS}")"
-echo "${GOOGLE_CREDENTIALS}" > /tmp/creds.json
-/google-cloud-sdk/bin/gcloud auth activate-service-account "${CLIENT_EMAIL}" --key-file=/tmp/creds.json
-
+google_auth_setup
 git_setup
 
 # check out the source branch at exactly the ref where we were triggered
 git checkout -b "${SOURCE_BRANCH}" "${GITHUB_SHA}"
 
 git remote add destination "${GOOGLE_SOURCE_REPO_URL}"
-git config --replace-all credential.helper '!/google-cloud-sdk/bin/gcloud auth git-helper --account='"${CLIENT_EMAIL}"' --ignore-unknown $@'
 
 git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" -f
